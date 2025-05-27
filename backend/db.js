441b4e39cd03e4b118384286d05262f54e11ed79
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-27 19:54:42 +0530

    Converted db url to .env

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { mongodb_url } = require ("./mongodbcofig.js")
+const mongodb_url  = process.env.mongodb_url;
 mongoose.connect(mongodb_url);
 
 const Userschema = new mongoose.Schema({
