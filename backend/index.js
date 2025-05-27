900c65c6980d21e76747b6016440df7993460f57
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-27 20:19:16 +0530

    Testing Server

--- backend/index.js
+++ backend/index.js
@@ -11,4 +11,7 @@ const mainRouter = require ('./routes/index.js');
 
 
 app.use("/api/v1", mainRouter)
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
