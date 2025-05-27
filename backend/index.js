f5afd4ca67f15f32910f4888982c867f41911ee8
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-27 20:11:19 +0530

    Minor changes to check deployment errors

--- backend/index.js
+++ backend/index.js
@@ -11,4 +11,4 @@ const mainRouter = require ('./routes/index.js');
 
 
 app.use("/api/v1", mainRouter)
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
